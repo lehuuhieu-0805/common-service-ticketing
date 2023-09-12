0ddb6c33378cd43cd05d5d6503daecc965b79111
lehuuhieu-0805 <01643815296h@gmail.com>
2023-09-13 05:47:26 +0700

    feat: add version for each data of event

--- src/events/order-created.event.ts
+++ src/events/order-created.event.ts
@@ -4,6 +4,7 @@ export interface OrderCreatedEvent {
   subject: Subjects.ORDER_CREATED;
   data: {
     id: string;
+    version: number;
     status: OrderStatus;
     userId: string;
     expiresAt: string;
